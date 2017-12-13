6e18a29f3c5cf7f760c02bc85c0cb46661c399e1
Syed Khurram <khurram@syedkhurram.com>
2017-12-13 17:04:52 +0100

    removed

--- doc/dist/latest/examples/resources/js/out/figwheel/connect/build_app.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-app (:require [antizer-examples.app] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:build-id "app", :websocket-url "ws://localhost:3451/figwheel-ws"})
-
